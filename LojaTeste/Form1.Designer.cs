bf27f9b06eb33df45611ce3142fecaf72af147f3
Eduardo <Eduardo@NOTEEDU>
2017-02-20 20:49:27 -0300

    Add project files.

---
+++ LojaTeste/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace LojaTeste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTexto = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TxtNome = new System.Windows.Forms.TextBox();
+            this.frnTeste2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTexto
+            // 
+            this.lblTexto.AutoSize = true;
+            this.lblTexto.ForeColor = System.Drawing.Color.Red;
+            this.lblTexto.Location = new System.Drawing.Point(27, 198);
+            this.lblTexto.Name = "lblTexto";
+            this.lblTexto.Size = new System.Drawing.Size(133, 13);
+            this.lblTexto.TabIndex = 0;
+            this.lblTexto.Text = "Meu Primeiro Programa C#";
+            this.lblTexto.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Tag = "Selecione o botão";
+            this.button1.Text = "Clique Aqui";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TxtNome
+            // 
+            this.TxtNome.Location = new System.Drawing.Point(30, 91);
+            this.TxtNome.Name = "TxtNome";
+            this.TxtNome.Size = new System.Drawing.Size(100, 20);
+            this.TxtNome.TabIndex = 2;
+            // 
+            // frnTeste2
+            // 
+            this.frnTeste2.Location = new System.Drawing.Point(132, 144);
+            this.frnTeste2.Name = "frnTeste2";
+            this.frnTeste2.Size = new System.Drawing.Size(108, 23);
+            this.frnTeste2.TabIndex = 3;
+            this.frnTeste2.Text = "Chama botão 2";
+            this.frnTeste2.UseVisualStyleBackColor = true;
+            this.frnTeste2.Click += new System.EventHandler(this.frnTeste2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(292, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.fileToolStripMenuItem.Text = "file";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.frnTeste2);
+            this.Controls.Add(this.TxtNome);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblTexto);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Teste";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTexto;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TxtNome;
+        private System.Windows.Forms.Button frnTeste2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+    }
+}
+
---
+++ LojaTeste/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LojaTeste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (TxtNome.Text.Equals(""))
+            {
+                MessageBox.Show("Informe o nome!");
+            }
+            else
+            {
+                MessageBox.Show("Olá mundo " + TxtNome.Text);
+                lblTexto.Text = "Olá mundo " + TxtNome.Text;
+                //FrmTela2.sh
+            }
+                           
+        }
+
+        private void frnTeste2_Click(object sender, EventArgs e)
+        {
+            Form tela = new FrmTela2();
+            tela.ShowDialog();
+            //this.Close();
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form tela = new FrmTela2();
+            tela.ShowDialog();
+        }
+    }
+}
