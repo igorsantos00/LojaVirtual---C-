46a1a9d3c8d48916714c256c10ddacdebf504968
igor.rrsantos <igor.rrsantos@CAS555588W0735>
2017-04-19 22:14:48 -0300

    Conectado com banco

---
+++ dataModel/clsConexao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataModel
+{
+    public class clsConexao
+    {
+       
+            public static SqlConnection Conectar()
+            {
+                SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder();
+                sb.UserID = "TSI";
+                sb.Password = "SIstemas123";
+                sb.DataSource = "assirati.database.windows.net";
+                sb.InitialCatalog = "Kanino";
+
+                SqlConnection cn = new SqlConnection(sb.ConnectionString);
+                cn.Open();
+                return cn;
+            }
+        }
+
+}
